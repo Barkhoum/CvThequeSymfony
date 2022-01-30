be7e503067bbce63545ea0d5cce42ffd6c0d0f78
Barkhoum <90609076+Barkhoum@users.noreply.github.com>
2022-01-30 21:55:04 +0100

    fin 29 Doctrine Repository find et Param convertor

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -16,6 +16,14 @@ class PersonneController extends AbstractController
         $personnes = $repository->findAll();
         return $this->render( 'personne/index.html.twig',['personnes'=> $personnes]);
     }
+    #[Route('/{id<\d+>}', name: 'personne.detail')]
+    public function detail(Personne $personne = null): Response{
+        if (!$personne){
+            $this->addFlash(type: 'error', message: "La personne n'existe pas !");
+            return $this->redirectToRoute('personne.list');
+        }
+        return $this->render( 'personne/detail.html.twig',['personne'=> $personne]);
+    }
     #[Route('/add', name: 'personne.add')]
     public function addPersonne(ManagerRegistry $doctrine): Response
     {
@@ -24,14 +32,11 @@ class PersonneController extends AbstractController
         $personne->setFirstname(firstname:'Guesmia');
         $personne->setName(name:'Barkhoum');
         $personne->setAge(age:'40');
-        //$personne2 = new Personne();
-        //$personne2->setFirstname(firstname:'Picard');
-        //$personne2->setName(name:'Tatiana');
-        //$personne2->setAge(age:'27');
+
 
         // ajouter l'operation d'insertion
         $entityManager->persist($personne);
-        $entityManager->persist($personne2);
+
 
         $entityManager->flush();
         return $this->render('personne/index.html.twig', [
