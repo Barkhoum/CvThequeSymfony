e7b472325fd3ae16500eac21aa604da2a355865c
Barkhoum <guesmia.barkhoum@gmzil.com>
2022-02-09 23:14:19 +0100

    fin du cours 42
     Les formulaires traitement de l'envoi du formulaire et ajout d'une personne

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -7,12 +7,13 @@ use App\Form\PersonneType;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 #[Route('personne')]
 class PersonneController extends AbstractController
 {
-    #[Route('/', name: 'personne.list.alls')]
+    #[Route('/alls', name: 'personne.alls')]
     public function index(ManagerRegistry $doctrine): Response{
 
         $repository = $doctrine->getRepository(Personne::class);
@@ -47,17 +48,29 @@ class PersonneController extends AbstractController
         return $this->render( 'personne/detail.html.twig',['personne'=> $personne]);
     }
     #[Route('/add', name: 'personne.add')]
-    public function addPersonne(ManagerRegistry $doctrine): Response
+    public function addPersonne(ManagerRegistry $doctrine, Request $request): Response
     {
-        $entityManager = $doctrine->getManager();
+
         $personne = new Personne();
         $form = $this->createForm(PersonneType::class,  $personne);
         $form->remove('createdAt');
         $form->remove('updatedAt');
+        //Mon formulaire va aller traiter la requête
+        $form->handleRequest($request);
+        //Estce que le formaulaire a été soumis
+        if($form->isSubmitted()){
+            $Manager = $doctrine->getManager();
+            $Manager->persist($personne);
+            $Manager->flush();
 
-        return $this->render(view: 'personne/add-personne.html.twig',
-            parameters: ['form' => $form->createView()
-            ]);
+
+            $this->addFlash($personne->getName(). "a été ajouté avec succès");
+            return $this->redirectToRoute('/');
+
+        }else{
+            return $this->render('personne/add-personne.html.twig', parameters: ['form' => $form->createView()
+                ]);
+        }
     }
     #[Route('/delete/{id}', name: 'personne.delete')]
     public function deletePersonne(Personne $personne = null, ManagerRegistry $doctrine): RedirectResponse{
@@ -66,17 +79,17 @@ class PersonneController extends AbstractController
             $manager = $doctrine->getManager();
             $manager->remove($personne); //Ajoute la fonction de suppression dans la transaction
             $manager->flush();// Exécuter la transaction
-            $this->addFlash(type:'success', message:'La personne a été supprimé avec succès');
+            $this->addFlash('success', 'La personne a été supprimé avec succès');
         //Si la personne existe => alors on va le supprimer et retourner un flashMessage de success
 
         }else{
             //sinon retourner un flashMessage d'erreur
-            $this->addFlash(type:'error', message:'Personne inexistante');
+            $this->addFlash('error', 'Personne inexistante');
              }
-        return $this->redirectToRoute(route: 'personne.list.alls');
+        return $this->redirectToRoute( 'personne.alls');
         }
 
-    #[Route('/update/{id}/{name}/{firstname}/{age}', name: 'personne.update')]
+    #[Route('/update/{id}/{name}/{firstname}/{age}', name:'personne.update')]
     public function updatePersonne(Personne $personne = null, ManagerRegistry $doctrine , $name, $firstname, $age){
     //Vefifier que la personne à mettre à jour existe
         if($personne) {
@@ -87,11 +100,11 @@ class PersonneController extends AbstractController
             $manager->persist($personne);
 
             $manager->flush();
-            $this->addFlash(type:"success", message:"La personne a été mise à jour avec succès");
+            $this->addFlash("success", "La personne a été mise à jour avec succès");
         }else{
 
-            $this->addFlash(type:"danger", message:"Personne innexistante");
+            $this->addFlash("danger", "Personne innexistante");
         }
-        return $this->redirectToRoute(route: 'personne.list.alls');
+        return $this->redirectToRoute( 'personne.alls');
     }
 }
